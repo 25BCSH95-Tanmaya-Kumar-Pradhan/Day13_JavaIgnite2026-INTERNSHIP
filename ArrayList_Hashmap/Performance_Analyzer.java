86a65d49661caf347b123fcb636cdb413ecab5f5
rite2harini <rite2harini@gmail.com>
2026-06-09 13:25:25 +0530

    Create Performance_Analyzer.java for student records
    Implemented a Java program to manage student course performance using ArrayList and HashMap.

---
+++ ArrayList_Hashmap/Performance_Analyzer.java
@@ -0,0 +1,19 @@
+/*
+Create a Java program to manage student course performance.
+
+Task:
+
+You are given multiple students with the following details:
+
+Student Name
+Course Name
+Marks
+Requirements:
+Store all student records using ArrayList
+Use a HashMap<String, List<Integer>> where:
+Key = Course Name
+Value = List of marks in that course
+Calculate and display:
+Average marks for each course
+Course with highest average
+*/
