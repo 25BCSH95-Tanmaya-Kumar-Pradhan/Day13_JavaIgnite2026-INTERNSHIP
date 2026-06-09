b3987bac0a04a5270bf327471fda4caf7b000562
rite2harini <rite2harini@gmail.com>
2026-06-09 13:19:22 +0530

    Create EmpDataAnalyzer.java

---
+++ ArrayList_Hashmap/EmpDataAnalyzer.java
@@ -0,0 +1,22 @@
+/*
+Create a program that stores employee data in memory:
+
+Each employee has:
+
+id
+department
+salary
+Task:
+Store employees using ArrayList
+Create a HashMap where:
+Key = Department
+Value = Total salary of that department
+Display department with highest total salary
+Hint:
+
+Break into:
+
+Grouping logic
+Summation logic
+Comparison logic
+*/
